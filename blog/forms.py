e3fd84d0e117c705b21fb151a828e6a94eea2dad
pythonteacher <myha@dimigo.hs.kr>
2015-11-08 09:55:36 +0900

    .

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    # 이 폼을 만들기 위해서 어떤 model이 쓰여야 하는지 장고에게 알려줌
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -11,3 +13,39 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('blog.views.post_detail', pk=post.pk)
+
+    else:
+        form = PostForm()   # 새 Post 폼을 추가하기 위해 PostForm() 함수를 호출하도록 하여 템플릿에 넘깁니다.
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+
+            return redirect('blog.views.post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/post_edit.html', {'form': form})
